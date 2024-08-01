233829adadcf9138d00fdb030b97f6ea1eff3742
dystaSatria <rezadysta@gmail.com>
2024-08-01 21:07:17 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple PHP Payment </title>
+</head>
+<body>
+
+    <form action="checkout.php" method="post"></form>
+    <h1>Simple PHP Payment </h1>
+    <p>T-Shirt</p>
+    <p><strong>$10 USD</strong></p>
+    <button>Pay</button>
+    
+</body>
+</html>
