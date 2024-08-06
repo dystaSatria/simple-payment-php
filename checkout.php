934b92dfd222807df196cdacaf09f0cedd995412
dystaSatria <rezadysta@gmail.com>
2024-08-06 13:26:19 +0300

    Nice GG

--- checkout.php
+++ checkout.php
@@ -1,6 +1,39 @@
-<?
+<?php
 
+require __DIR__ . "/vendor/autoload.php";
 
-require __DIR__ . '/vendor/autoload.php';
+$stripe_secret_key = "sk_test_51PkSqGIai1Xe1X0wvnvUVyuR9O9qo9MOOvyMH9dgLzfnPSxEozwKAFiQomYkF8G9PQMJB3oVAdiVzk90uWIWZDNy00u9Vugv5j";
 
-$stripe_secret_key =
+\Stripe\Stripe::setApiKey($stripe_secret_key);
+
+$checkout_session = \Stripe\Checkout\Session::create([
+    "mode" => "payment",
+    "success_url" => "http://localhost/success.php",
+    "cancel_url" => "http://localhost/index.php",
+    "locale" => "auto",
+    "line_items" => [
+        [
+            "quantity" => 1,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 2000,
+                "product_data" => [
+                    "name" => "T-shirt"
+                ]
+            ]
+        ],
+        [
+            "quantity" => 2,
+            "price_data" => [
+                "currency" => "usd",
+                "unit_amount" => 700,
+                "product_data" => [
+                    "name" => "Hat"
+                ]
+            ]
+        ]        
+    ]
+]);
+
+http_response_code(303);
+header("Location: " . $checkout_session->url);
--- succes.php
+++ succes.php
@@ -7,10 +7,6 @@
 <body>
 
     <h1>Stripe Example</h1>
-    <form method="post" action="checkout.php">
-        <p>T-shirt</p>
-        <p><strong>US$20.00</strong></p>
-        <button>Pay</button>
-    </form>
+    <p>Thank you for your payment!</p>
 </body>
 </html>
